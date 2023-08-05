d06febf890dd1d560e3cc9c99b50e9e92b82b14c
Francesca Santoriello <f.santoriello@hotmail.com>
2023-08-05 21:01:06 +0200

    phoneAuth (backup captcha bug)

--- src/components/LoginBtn.vue
+++ src/components/LoginBtn.vue
@@ -13,17 +13,17 @@
                 {{ newUser ? 'SIGNUP' : 'LOGIN' }}
             </button>
         </div>
-        <div>
+        <!-- <div>
             <div v-if="confirmationResult">
                 <input v-model="verificationCode" type="text" placeholder="Verification Code" class="input">
-                <button @click="verifyCode()" class="button">Verify</button>
+                <button @click="verifyCode()" :class="{ 'is-loading': loading }" class="button">Verify</button>
             </div>
             <div v-else>
                 <input v-model="phoneNumber" type="tel" placeholder="number" class="input">
-                <button @click="sendVerificationCode" class="button">Send Code</button>
+                <button @click="sendVerificationCode" :class="{ 'is-loading': loading }" id="send-captcha" class="button">Send Code</button>
             </div>
             <div id="recaptcha-container"></div>
-        </div>
+        </div> -->
         <p class="has-text-danger" v-if="errorMessage">
             {{ errorMessage }}
         </p>
@@ -32,9 +32,10 @@
 
 <script>
 import { defineComponent } from 'vue'
-import { auth, RecaptchaVerifier } from '../firebase'
+import { auth } from '../firebase'
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth'
-import { signInWithPhoneNumber } from 'firebase/auth'
+// import { RecaptchaVerifier } from '../firebase'
+// import { signInWithPhoneNumber } from 'firebase/auth'
 
 export default defineComponent({    
     data() {
@@ -46,15 +47,14 @@ export default defineComponent({
             loading: false,
             errorMessage: '',
             phoneNumber: '',
-            verificationCode: '',
-            confirmationResult: null
+            // verificationCode: '',
+            // confirmationResult: null
         }
     },
     methods: {
         async signInOrCreateUser() {
             this.loading = true
             this.errorMessage = ''
-            
             try {
                 if (this.newUser) {
                     await createUserWithEmailAndPassword(auth, this.email, this.password)
@@ -64,34 +64,36 @@ export default defineComponent({
             } catch(error) {
                 this.errorMessage = error.message
             }
-            
             this.loading = false
         },
-        async sendVerificationCode() {
-            try {
-                const appVerifier = new RecaptchaVerifier('recaptcha-container', {
-                    'size': 'normal',
-                    'callback': () => {
-                        this.onSignInSubmit();
-                    }
-                });
-                
-                this.confirmationResult = await signInWithPhoneNumber(auth, this.phoneNumber, appVerifier);
-            } catch (error) {
-                console.error('Error sending verification code:', error);
-            }
-        },
-        async verifyCode() {
-            try {
-                const userCredential = await this.confirmationResult.confirm(this.verificationCode);
-                const user = userCredential.user;
-                console.log('Phone number signup successful:', user);
-            } catch (error) {
-                console.error('Error verifying code:', error);
-            }
-        },
-        onSignInSubmit() {
-        }    
+        // async sendVerificationCode() {
+        //     this.loading = true
+        //     try {
+        //         const appVerifier = new RecaptchaVerifier('send-captcha', {
+        //             'size': 'normal',
+        //             'callback': () => {
+        //                 this.onSignInSubmit();
+        //             }
+        //         });
+        //         this.confirmationResult = await signInWithPhoneNumber(auth, this.phoneNumber, appVerifier);
+        //     } catch (error) {
+        //         console.error('Error sending verification code:', error);
+        //     }
+        //     this.loading = false
+        // },
+        // async verifyCode() {
+        //     this.loading = true
+        //     try {
+        //         const userCredential = await this.confirmationResult.confirm(this.verificationCode);
+        //         const user = userCredential.user;
+        //         console.log('Phone number signup successful:', user);
+        //     } catch (error) {
+        //         console.error('Error verifying code:', error);
+        //     }
+        //     this.loading = false
+        // },
+        // onSignInSubmit() {
+        // }    
     }
 })
 </script>
