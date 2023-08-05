9540c39d414946ee958c6c51691c6cb366b06aae
Francesca Santoriello <f.santoriello@hotmail.com>
2023-08-05 18:11:25 +0200

    login/out logic

--- src/components/HomeScreen.vue
+++ src/components/HomeScreen.vue
@@ -1,26 +1,28 @@
 <template>
     <div>
         <h3>Home</h3>
-
+        
         <CurrentUser>
             <template v-slot:user="{user}">
                 <div v-if="user">
-                    <p>Hi {{ user.uid }}</p>
-                </div>
-                <LoginBtn v-else />
-            </template>
-        </CurrentUser>
-    </div>
-</template>
-
-<script>
-import LoginBtn from './LoginBtn'
-import CurrentUser from './CurrentUser'
-
-export default {
-    components: {
-        LoginBtn,
-        CurrentUser
+                    <UserProfile :user="user" />
+                    </div>
+                    <LoginBtn v-else />
+                </template>
+            </CurrentUser>
+        </div>
+    </template>
+    
+    <script>
+    import LoginBtn from './LoginBtn'
+    import CurrentUser from './CurrentUser'
+    import UserProfile from './UserProfile.vue'
+    
+    export default {
+        components: {
+            LoginBtn,
+            CurrentUser,
+            UserProfile
+        }
     }
-}
 </script>
---
+++ src/components/UserProfile.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <p>Hi {{ user.uid }}</p>
+        <button @click="auth.signOut()">ByeBye</button>
+    </div>
+</template>
+
+<script>
+import { auth } from '../firebase'
+
+export default {
+    data() {
+        return {
+            auth
+        }
+    },
+    props: ['user']
+}
+</script>
