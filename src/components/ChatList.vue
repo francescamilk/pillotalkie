2c6bc8874538c3977d891624cff6161ef03a3b66
Francesca Santoriello <f.santoriello@hotmail.com>
2023-08-05 22:47:00 +0200

    ChatList createDoc AT2.1

--- src/components/ChatList.vue
+++ src/components/ChatList.vue
@@ -5,23 +5,20 @@
 </template>
 
 <script>
-import { db } from '../firebase'
-import { ref, push } from 'firebase/database'
+import { getDatabase, ref, set } from 'firebase/database'
 
 export default {
     methods: {
         async createChat() {
             try {
-                const chatsRef = ref(db, 'chats')
-                const newChatRef = push(chatsRef)
-                
-                await newChatRef.set({
+                const db = getDatabase()
+                const chat = set(ref(db, 'chats/' + this.uid), {
                     createdAt: Date.now(),
                     owner: this.uid,
-                    members: [this.uid],
+                    members: [this.uid]
                 })
                 
-                console.log('Chat room created with key:', newChatRef.key)
+                console.log('Chat room created with key:', chat.id)
             } catch (error) {
                 console.error('Error creating chat room:', error)
             }
