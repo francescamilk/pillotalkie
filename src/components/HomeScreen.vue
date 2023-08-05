e131dffd3a08c35cbcd6cef500c9eb1172b4bb3a
Francesca Santoriello <f.santoriello@hotmail.com>
2023-08-05 14:24:40 +0200

    test auth.signInAnon

--- src/components/HomeScreen.vue
+++ src/components/HomeScreen.vue
@@ -1,5 +1,17 @@
 <template>
     <div>
         <h3>Home</h3>
+
+        <LoginBtn />
     </div>
 </template>
+
+<script>
+import LoginBtn from './LoginBtn'
+
+export default {
+    components: {
+        LoginBtn
+    }
+}
+</script>
---
+++ src/components/LoginBtn.vue
@@ -0,0 +1,30 @@
+<template>
+    <aside>
+        <button class="button" @click="loginAnonimous()">SignIn</button>
+    </aside>
+</template>
+
+<script>
+import { auth } from '../firebase'
+import { signInAnonymously } from 'firebase/auth'
+
+export default {
+    data() {
+        return {
+            auth
+        }
+    },
+    methods: {
+        loginAnonimous() {
+            signInAnonymously(auth)
+            .then(() => {
+                // Signed in
+            })
+            // .catch((error) => {
+            //     // const errorCode = error.code;
+            //     // const errorMessage = error.message;
+            // });
+        }
+    }
+}
+</script>
